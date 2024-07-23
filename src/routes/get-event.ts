cd5bcc3d58730ef0a48fffebf4f312254a2c3c3d
João Silva <jpsouzadasilva@gmail.com>
2024-07-23 19:13:46 -0300

    fix: get-event response

--- src/routes/get-event.ts
+++ src/routes/get-event.ts
@@ -9,42 +9,53 @@ export async function getEvent(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .get('/events/:eventId', {
-    schema: {
-      params: z.object({
-        eventId: z.string().uuid()
-      }),
-      response: {
-        200: z.object({
-          id: z.string().uuid(),
-          title: z.string(),
-          slug: z.string(),
-          details: z.string().nullable(),
-          maximumAttendees: z.number().int().nullable(),
-          attendeeAmount: z.number(),
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid()
         }),
-        404: z.object({
-          message: z.string()
-        })
+        response: {
+          200: z.object({
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              slug: z.string(),
+              details: z.string().nullable(),
+              maximumAttendees: z.number().int().nullable(),
+              attendeeAmount: z.number(),
+            })
+          }),
+          404: z.object({
+            message: z.string()
+          })
+        }
       }
-    }
-  }, async (request, reply) => {
-    const { eventId } = request.params
+    }, async (request, reply) => {
+      const { eventId } = request.params;
 
-    const eventRepository = new EventRepository()
-    const getEventUseCase = new GetEventUseCase(eventRepository)
-    
-    try {
-      const event = await getEventUseCase.execute({eventId})
+      const eventRepository = new EventRepository();
+      const getEventUseCase = new GetEventUseCase(eventRepository);
 
-      reply.status(200).send(event)
-    } catch (error) {
-      if (error instanceof EventNotFoundError) {
-        reply.status(404).send({ message: error.message })
+      try {
+        const eventData = await getEventUseCase.execute({ eventId });
+
+        const response = {
+          event: {
+            id: eventData.id,
+            title: eventData.title,
+            slug: eventData.slug,
+            details: eventData.details,
+            maximumAttendees: eventData.maximumAttendees,
+            attendeeAmount: eventData.attendeeAmount
+          }
+        };
+
+        reply.status(200).send(response);
+      } catch (error) {
+        if (error instanceof EventNotFoundError) {
+          return reply.status(404).send({ message: error.message });
+        }
+        
+        reply.status(500).send({ message: 'Internal Server Error' });
       }
-      
-      reply.status(500).send({ message: 'Internal Server Error'})
-    }
-
-
-  })
+    });
 }
